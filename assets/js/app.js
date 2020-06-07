5a3f51ee982b9ff812592d3b219f76bc159e0a88
coderrooster <abitohari@gmail.com>
2020-06-07 17:04:51 +0700

    add app.js with vue script

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+var app = new Vue({
+  el: "#app",
+  data() {
+    return {
+      appTitle : "Coder Rooster Blog",
+      appDesc : {
+        status : true,
+        parent : "Hi guys!",
+        child : "<i>Raiso turu aku ^_^</i>", //use v-html='appDesc.child'
+      },
+      //data computed
+      int1 : 5,
+      int2 : 3,
+      intResult: null,
+
+    }
+  },
+  computed: {
+    sum: function () {
+      return this.int1 + this.int2;
+    }
+  },
+  methods: {
+    sumProcess: function(){
+      return this.intResult = this.int2 + this.int2;
+    }
+  },
+
+})
